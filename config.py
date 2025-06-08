2f49a14c23d70b72bf4f17ca1c3911f99fc7263c
sudiproyal1 <royalstag1321@gmail.com>
2025-06-09 02:55:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 MONGO_URI = os.getenv("MONGO_URI")
-ADMIN_IDS = [123456789]  # Replace with your Telegram user ID(s)
+ADMIN_IDS = [6535216093]  # Replace with your Telegram user ID(s)
