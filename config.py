1e3bc9a2b8c42982a20b441d08202a5c9fad5f4c
sudiproyal1 <royalstag1321@gmail.com>
2025-06-09 03:02:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-MONGO_URI = os.getenv("MONGO_URI")
+BOT_TOKEN = os.getenv("7620053279:AAGETQR-9A_KUhAcldUqz41EckEvBRfgpSE")
+MONGO_URI = os.getenv("mongodb+srv://royalstag1321:8bKKKWWOd4bNgS3N@matefinderbot.gnu1ds9.mongodb.net/?retryWrites=true&w=majority&appName=MateFinderBot")
 ADMIN_IDS = [6535216093]  # Replace with your Telegram user ID(s)
